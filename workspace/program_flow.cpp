28a2a24677df6507ba3aa6f6379dca5b026790b4
unknown <guswns3753@gmail.com>
2026-05-07 09:35:32 +0900

    first commit

---
+++ workspace/program_flow.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int age = 10;
+	char first = 'A';
+	std::cout << "Hello " << age << first << std::endl; //2026/05/06 17:00 수업중 using 사례 제시 -> 원상복귀
+	//cout << "Hello" << endl; //2026/05/06 17:00 수업중 확인
+    return 0;
+}
